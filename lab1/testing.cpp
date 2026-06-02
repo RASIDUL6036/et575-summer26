09555ed99c4175eb314462949ecbcf233c838e6b
RASIDUL6036 <m7hp5r4cth@privaterelay.appleid.com>
2026-06-02 05:49:10 +0000

    start lab 2

--- lab1/testing.cpp
+++ lab1/testing.cpp
@@ -5,10 +5,10 @@ int main() {
 
     string username = "";
 
-    cout << "Md Rasidul: ";
+    cout << "Enter your full name:Md Rasidul ";
     cin >> username;
 
-    cout << "Welcome to C++ programming Mf Rasidul "
+    cout << "Welcome to C++ programming Md Rasidul "
          << username << endl;
 
     return 0;
