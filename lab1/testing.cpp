53704324ef573e4185d3c298a0a6eef7bfb5b361
RASIDUL6036 <m7hp5r4cth@privaterelay.appleid.com>
2026-06-02 05:44:07 +0000

    start lab 2

---
+++ lab1/testing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+    string username = "";
+
+    cout << "Md Rasidul: ";
+    cin >> username;
+
+    cout << "Welcome to C++ programming Mf Rasidul "
+         << username << endl;
+
+    return 0;
+}
